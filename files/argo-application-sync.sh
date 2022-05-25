0afd1187cf2fd2773327e3a1a43254e3a60e2fa3
fhochleitner <felix.hochleitner@outlook.com>
2022-05-25 08:11:01 +0200

    GPX-335 Cluster-Setup Aufteilung
    zusätzliches Warten darauf, dass alle Nodes und Pods tatsächlich fertig sind.
    
    Signed-off-by: fhochleitner <felix.hochleitner@outlook.com>

--- files/argo-application-sync.sh
+++ files/argo-application-sync.sh
@@ -9,6 +9,14 @@ function getNotHealthy() {
   oc get applications --insecure-skip-tls-verify -o jsonpath='{.items[*].status.health}' --kubeconfig="${KUBECONFIG}" -n openshift-gitops | jq 'select(.status != "Healthy")' -c | wc -l
 }
 
+function getNodesNotReady() {
+  oc get nodes --insecure-skip-tls-verify --o jsonpath="{range .items[?(@.status.conditions[-1].type!='Ready')]}{.metadata.name} {.status.conditions[-1].type}{'\n'}{end}" | wc -l
+}
+
+function getPendingPods() {
+   oc get pods --insecure-skip-tls-verify -A --field-selector status.phase=Pending --kubeconfig="${KUBECONFIG}" | wc -l
+}
+
 function waitForSync() {
   # Wait for all Applications to finish syncing
   echo "waiting for applications to be synced"
@@ -52,6 +60,32 @@ function waitForHealthy() {
   fi
 }
 
+function waitForNodesReady() {
+  while test "${not_ready_nodes}" -gt 0 && test "${loops}" -lt 30; do
+    sleep 60
+    loops=$((loops+1))
+    not_ready_nodes=$(getNodesNotReady)
+  done;
+  if test "${not_ready_nodes}" -gt 0; then
+    echo "Nodes not ready after 30 Minutes. Exiting!"
+    exit 1
+  fi
+}
+
+function waitForPodsReady() {
+  while test "${not_ready_pods}" -gt 0 && test "${loops}" -lt 30; do
+    sleep 60
+    loops=$((loops+1))
+    not_ready_pods=$(getPendingPods)
+  done;
+
+  if test "${not_ready_pods}" -gt 0; then
+    echo "Pods not ready after 30 Minutes. Exiting!"
+    exit 1
+  fi
+}
+
+
 function main() {
   KUBECONFIG=$1
 
@@ -87,6 +121,15 @@ function main() {
     echo "Operations not finished in time. Exiting"
     exit 1
   fi
+
+  sleep 300 # wait for 10 minutes to be sure that nodes are rebooted and applications are actually ready.
+  not_ready_nodes=$(getNodesNotReady)
+  waitForNodesReady
+
+  sleep 180 # safety first
+  not_ready_pods=$(getPendingPods)
+  waitForPodsReady
+
 }
 
 main "${1}"
